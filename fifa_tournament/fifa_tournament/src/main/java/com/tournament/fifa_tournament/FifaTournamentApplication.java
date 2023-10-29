7370aa2984178b3b3c396f89c94176f30273b19f
DESKTOP-LSAQ3SJ\Admin <sarockylukas@gmail.com>
2023-10-29 17:31:07 +0100

    init

---
+++ fifa_tournament/fifa_tournament/src/main/java/com/tournament/fifa_tournament/FifaTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.tournament.fifa_tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaTournamentApplication.class, args);
+	}
+
+}
---
+++ fifa_tournament/fifa_tournament/src/test/java/com/tournament/fifa_tournament/FifaTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tournament.fifa_tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
